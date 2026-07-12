68c60a284d9bb3d9253d446eade74c7eae7e79fb
Lynan <lynanbreeze@gmail.com>
2026-07-12 16:40:39 +0800

    style: update Clear requests icon

--- devtools-panel.js
+++ devtools-panel.js
@@ -906,10 +906,11 @@
           <nav>
             <button type="button" data-clear class="icon-btn" title="Clear requests">
               <svg viewBox="0 0 24 24" stroke="currentColor" stroke-width="2.2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <polyline points="3 6 5 6 21 6"></polyline>
-                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                <line x1="10" y1="11" x2="10" y2="17"></line>
-                <line x1="14" y1="11" x2="14" y2="17"></line>
+                <path d="M19 5L12 12"></path>
+                <path d="M12 12L7 14l-4 6a1 1 0 0 0 1 1.5h8a1 1 0 0 0 1-1.2l-2-4.3z"></path>
+                <path d="M8 15.5l-3.5 4.5"></path>
+                <path d="M9.5 15l-1.5 5"></path>
+                <path d="M11 14.5l0.5 5.5"></path>
               </svg>
             </button>
             <button type="button" data-close class="icon-btn close-btn" title="Collapse panel">
